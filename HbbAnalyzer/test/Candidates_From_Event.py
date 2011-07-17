200d3a2096e022406588907b66e6f3c7273d0d96
tboccali <>
2011-07-17 12:36:11 +0000

    add test to go VHbbEvent->VHbbCandidates

---
+++ HbbAnalyzer/test/Candidates_From_Event.py
@@ -0,0 +1,41 @@
+#import configurations
+import FWCore.ParameterSet.Config as cms
+
+import os 
+
+
+# define the process
+process = cms.Process("Candidates")
+process.source = cms.Source("PoolSource",
+ fileNames = cms.untracked.vstring('file:/gpfs/gpfsddn/cms/user/boccali/hbb/ttjets/VHbbPAT.edm_5_0_eqA.root') ) 
+
+
+process.out = cms.OutputModule(
+    'PoolOutputModule',
+    fileName       = cms.untracked.string('Cand.root'),
+    outputCommands = cms.untracked.vstring('drop *','keep *_HbbAnalyzerNew_*_*', 'keep *_hbbCandidates_*_*'),
+    dropMetaData = cms.untracked.string('ALL'),
+    splitLevel = cms.untracked.int32(0)
+    )
+
+
+process.hbbCandidates = cms.EDProducer("HbbCandidateFinder",
+				       VHbbEventLabel = cms.InputTag(""),
+				       verbose = cms.bool(False) ,
+				       jetPtThreshold = cms.double(30.)
+				      )
+
+
+# drop the meta data for dropped data
+#process.out.dropMetaData = cms.string("DROPPED")
+
+#process.out.fileName = '/tigress-hsm/dlopes/PatEDM.root'
+
+
+# define path 'p'
+process.p = cms.Path(	     process.hbbCandidates                     )
+
+
+process.e = cms.EndPath(process.out)
+
+#
