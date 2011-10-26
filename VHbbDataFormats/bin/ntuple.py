ce4cc8d210cbdc091bc27e616c941f808eb25c0c
arizzi <>
2011-10-26 08:55:48 +0000

    use 36 bins

--- VHbbDataFormats/bin/ntuple.py
+++ VHbbDataFormats/bin/ntuple.py
@@ -7,14 +7,22 @@ process = cms.Process("FWLitePlots")
 #fileNames   = cms.vstring('file:2l2bMetEdmNtuples.root'),         ## mandatory
 process.fwliteInput = cms.PSet(
     fileNames   = cms.vstring(
-"/tmp/arizzi/PAT.edm_2_1_a51.root"
+"root://cmsdcache//pnfs/pi.infn.it/data/cms/store/user/arizzi/WH_WToLNu_HToBB_M-110_7TeV-powheg-herwigpp/HBB_EDMNtupleV11_ProcV1/68dcc5b95bb3c453e1bd27a02f6327b5//PAT.edm_1_3_00h.root",
+"root://cmsdcache//pnfs/pi.infn.it/data/cms/store/user/arizzi/WH_WToLNu_HToBB_M-110_7TeV-powheg-herwigpp/HBB_EDMNtupleV11_ProcV1/68dcc5b95bb3c453e1bd27a02f6327b5//PAT.edm_2_1_ov7.root",
+"root://cmsdcache//pnfs/pi.infn.it/data/cms/store/user/arizzi/WH_WToLNu_HToBB_M-110_7TeV-powheg-herwigpp/HBB_EDMNtupleV11_ProcV1/68dcc5b95bb3c453e1bd27a02f6327b5//PAT.edm_3_1_7er.root",
+"root://cmsdcache//pnfs/pi.infn.it/data/cms/store/user/arizzi/WH_WToLNu_HToBB_M-110_7TeV-powheg-herwigpp/HBB_EDMNtupleV11_ProcV1/68dcc5b95bb3c453e1bd27a02f6327b5//PAT.edm_4_1_3Yw.root",
+"root://cmsdcache//pnfs/pi.infn.it/data/cms/store/user/arizzi/WH_WToLNu_HToBB_M-110_7TeV-powheg-herwigpp/HBB_EDMNtupleV11_ProcV1/68dcc5b95bb3c453e1bd27a02f6327b5//PAT.edm_5_1_Ren.root",
+"root://cmsdcache//pnfs/pi.infn.it/data/cms/store/user/arizzi/WH_WToLNu_HToBB_M-110_7TeV-powheg-herwigpp/HBB_EDMNtupleV11_ProcV1/68dcc5b95bb3c453e1bd27a02f6327b5//PAT.edm_6_1_axp.root",
+"root://cmsdcache//pnfs/pi.infn.it/data/cms/store/user/arizzi/WH_WToLNu_HToBB_M-110_7TeV-powheg-herwigpp/HBB_EDMNtupleV11_ProcV1/68dcc5b95bb3c453e1bd27a02f6327b5//PAT.edm_7_1_gId.root",
+"root://cmsdcache//pnfs/pi.infn.it/data/cms/store/user/arizzi/WH_WToLNu_HToBB_M-110_7TeV-powheg-herwigpp/HBB_EDMNtupleV11_ProcV1/68dcc5b95bb3c453e1bd27a02f6327b5//PAT.edm_8_1_h1I.root"
+
 ###/gpfs/gpfsddn/cms/user/arizzi/Hbb/V9/CMSSW_4_2_8_patch3/src/VHbbAnalysis/HbbAnalyzer/test/PAT.edm.root"
 
 ),
 
-    PUmcfileName = cms.string("ttbarPU.root"),
+    PUmcfileName = cms.string("ttbarPU_36bins.root"),
     PUdatafileName2011B = cms.string("Cert_175832-178078_7TeV_PromptReco_Collisons11_JSON.pileupTruth_v2.root"),
-    PUdatafileName = cms.string("Pileup_2011_to_173692_LPLumiScale_68mb_51bins.root"),
+    PUdatafileName = cms.string("Pileup_2011_to_173692_LPLumiScale_68mb_36bins.root"),
     maxEvents   = cms.int32(-1),                             ## optional
     skipEvents   = cms.int32(0),                             ## optional
     outputEvery = cms.uint32(0),                            ## optional
