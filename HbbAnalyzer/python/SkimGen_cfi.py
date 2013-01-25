0db42640e3dd67dd3983afe6d999cc2a44168c85
jiafulow <>
2013-01-25 01:50:20 +0000

    Removed wrongly addeded plugins/SkimGen.cc and python/SkimGen_cfi.py

--- HbbAnalyzer/python/SkimGen_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-skim = cms.EDFilter("SkimGen",
-    mcTruthCollection = cms.InputTag("genParticles"),
-    genpart=cms.vint32(22), # photons
-    genpt=cms.double(50.),
-    Debug=cms.bool(False)
-)
