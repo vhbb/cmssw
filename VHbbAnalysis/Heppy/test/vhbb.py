c079ca5fe302a91b7a31fee03e6cf9d453ef3845
Joosep Pata <joosep.pata@cern.ch>
2015-02-26 17:22:51 +0100

    reverted commit to vhbb concerning profiling

--- VHbbAnalysis/Heppy/test/vhbb.py
+++ VHbbAnalysis/Heppy/test/vhbb.py
@@ -237,13 +237,10 @@ if __name__ == '__main__':
     from PhysicsTools.HeppyCore.framework.looper import Looper 
     looper = Looper( 'Loop', config, nPrint = 1, nEvents = 100)
 
-    # for a in looper.analyzers:
-    #     print a.name
-    # import time
-    # import cProfile
+    import time
+    import cProfile
+    p = cProfile.Profile(time.clock)
+    p.runcall(looper.loop)
+    p.print_stats()
     looper.loop()
-    #p = cProfile.Profile(time.clock)
-    #p.runcall(looper.loop)
-    #p.print_stats()
-#    looper.loop()
     looper.write()
