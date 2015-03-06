c56bf8edbb5b40c874bf8c1749d97ee57f23b9a7
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2015-03-06 10:55:35 +0100

    Moved boosted-scheduling to vhbb-combined.py

--- VHbbAnalysis/Heppy/test/vhbb-combined.py
+++ VHbbAnalysis/Heppy/test/vhbb-combined.py
@@ -4,17 +4,41 @@ from vhbb import *
 from VHbbAnalysis.Heppy.AdditionalBTag import AdditionalBTag
 from VHbbAnalysis.Heppy.AdditionalBoost import AdditionalBoost
 
+
+# Add Boosted Information
+
 boostana=cfg.Analyzer(
     verbose=False,
     class_object=AdditionalBoost,
 )
 sequence.insert(sequence.index(VHbb),boostana)
 
+treeProducer.collections["ungroomedFatjets"] = NTupleCollection("ungroomedFatjets", 
+                                                                fatjetType, 
+                                                                10,
+                                                                help = "CA, R=1.5, pT > 200 GeV, no grooming")
+
+treeProducer.collections["trimmedFatjets"] = NTupleCollection("trimmedFatjets",
+                                                              fourVectorType,
+                                                              10,
+                                                              help="CA, R=1.5, pT > 200 GeV, trimmed with R=0.2 and f=0.06")
+
+treeProducer.collections["httCandidates"] = NTupleCollection("httCandidates",
+                                                             httType,
+                                                             10,
+                                                             help="MultiR HEPTopTagger Candidates")
+
+
+# Add b-Tagging Information
+
 btagana=cfg.Analyzer(
     verbose=False,
     class_object=AdditionalBTag,
 )
 sequence.insert(sequence.index(VHbb),btagana)
+
+
+# Run Everything
 
 preprocessor = CmsswPreprocessor("combined_cmssw.py")
 config.preprocessor=preprocessor
