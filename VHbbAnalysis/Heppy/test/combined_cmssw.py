9edbda7b954ee8ed6d69e17fb1e0b909b3146aa4
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2015-05-19 11:47:49 +0200

    Add AK08 fatjets, different groomings, change naming of fatjet branches

--- VHbbAnalysis/Heppy/test/combined_cmssw.py
+++ VHbbAnalysis/Heppy/test/combined_cmssw.py
@@ -109,6 +109,16 @@ process.looseOptRHTT = cms.EDProducer(
 process.looseOptRHTT.src = cms.InputTag("chs")
 process.looseOptRHTT.jetPtMin = cms.double(200.)
 
+# AntiKt, R=0.8, pT > 200 GeV
+process.ak08PFJetsCHS = cms.EDProducer(
+        "FastjetJetProducer",
+        PFJetParameters,
+        AnomalousCellParameters,
+        jetAlgorithm = cms.string("AntiKt"),
+        rParam       = cms.double(0.8))
+process.ak08PFJetsCHS.src = cms.InputTag("chs")
+process.ak08PFJetsCHS.jetPtMin = cms.double(200.)
+
 # CA, R=1.5, pT > 200 GeV
 process.ca15PFJetsCHS = cms.EDProducer(
         "FastjetJetProducer",
@@ -136,6 +146,53 @@ process.ca15PFJetsCHSNSubjettiness  = cms.EDProducer("NjettinessAdder",
                                                      akAxesR0 = cms.double(-999.0)        # not used by default
 )
 
+# Calculate tau1, tau2 and tau3 for ungroomed AK R=0.8 jets
+process.ak08PFJetsCHSNSubjettiness  = cms.EDProducer("NjettinessAdder",
+                                                     src=cms.InputTag("ak08PFJetsCHS"),
+                                                     cone=cms.double(0.8),
+                                                     Njets = cms.vuint32(1,2,3),
+                                                     # variables for measure definition : 
+                                                     measureDefinition = cms.uint32( 0 ), # CMS default is normalized measure
+                                                     beta = cms.double(1.0),              # CMS default is 1
+                                                     R0 = cms.double(0.8),                # CMS default is jet cone size
+                                                     Rcutoff = cms.double( -999.0),       # not used by default
+                                                     # variables for axes definition :
+                                                     axesDefinition = cms.uint32( 6 ),    # CMS default is 1-pass KT axes
+                                                     nPass = cms.int32(-999),             # not used by default
+                                                     akAxesR0 = cms.double(-999.0)        # not used by default
+)
+
+# Apply pruning to AK R=0.8 jets
+process.ak08PFPrunedJetsCHS = process.ak08PFJetsCHS.clone(
+    usePruning = cms.bool(True),
+    nFilt = cms.int32(2),
+    zcut = cms.double(0.1),
+    rcut_factor = cms.double(0.5),
+    useExplicitGhosts = cms.bool(True))
+
+# Apply softdrop to AK R=0.8 jets
+process.ak08PFSoftdropJetsCHS = process.ak08PFJetsCHS.clone(
+    useSoftDrop = cms.bool(True),
+    zcut = cms.double(0.1),
+    beta = cms.double(0.0),
+    R0 = cms.double(0.8),
+    useExplicitGhosts = cms.bool(True))
+
+# Apply pruning to CA R=1.5 jets
+process.ca15PFPrunedJetsCHS = process.ca15PFJetsCHS.clone(
+    usePruning = cms.bool(True),
+    nFilt = cms.int32(2),
+    zcut = cms.double(0.1),
+    rcut_factor = cms.double(0.5),
+    useExplicitGhosts = cms.bool(True))
+
+# Apply softdrop to CA R=1.5 jets
+process.ca15PFSoftdropJetsCHS = process.ca15PFJetsCHS.clone(
+    useSoftDrop = cms.bool(True),
+    zcut = cms.double(0.1),
+    beta = cms.double(0.0),
+    R0 = cms.double(1.5),
+    useExplicitGhosts = cms.bool(True))
 
 # Apply trimming to CA R=1.5 jets
 process.ca15PFTrimmedJetsCHS = process.ca15PFJetsCHS.clone(
@@ -144,10 +201,17 @@ process.ca15PFTrimmedJetsCHS = process.ca15PFJetsCHS.clone(
     trimPtFracMin = cms.double(0.06),
     useExplicitGhosts = cms.bool(True))
 
+process.OUT.outputCommands.append("keep *_ak08PFJetsCHS_*_EX")
 process.OUT.outputCommands.append("keep *_ca15PFJetsCHS_*_EX")
-process.OUT.outputCommands.append("keep *_ca15PFJetsCHSNSubjettiness_*_EX")
+process.OUT.outputCommands.append("keep *_ak08PFPrunedJetsCHS_*_EX")
+process.OUT.outputCommands.append("keep *_ca15PFPrunedJetsCHS_*_EX")
+process.OUT.outputCommands.append("keep *_ak08PFSoftdropJetsCHS_*_EX")
+process.OUT.outputCommands.append("keep *_ca15PFSoftdropJetsCHS_*_EX")
 process.OUT.outputCommands.append("keep *_ca15PFTrimmedJetsCHS_*_EX")
+process.OUT.outputCommands.append("keep *_ak08PFJetsCHSNSubjettiness_*_EX")
+process.OUT.outputCommands.append("keep *_ca15PFJetsCHSNSubjettiness_*_EX")
 process.OUT.outputCommands.append("keep *_looseOptRHTT_*_EX")
+
 
 ########################################
 # Generator level hadronic tau decays
