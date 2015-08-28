8a037a9c591a46572006c71df77bc779f447717a
Andrea <andrea.rizzi@cern.ch>
2015-08-28 12:10:20 +0200

    properly implement running on data

---
+++ VHbbAnalysis/Heppy/test/vhbb-combined-data.py
@@ -0,0 +1,20 @@
+from vhbb-combined import *
+
+sample.isMC=False
+sample.isData=True
+sample.files=[
+ "root://xrootd.unl.edu//store/data/Run2015B/SingleMuon/MINIAOD/PromptReco-v1/000/251/162/00000/160C08A3-4227-E511-B829-02163E01259F.root"
+]
+FlagsAna.processName='RECO'
+
+# and the following runs the process directly 
+if __name__ == '__main__':
+    from PhysicsTools.HeppyCore.framework.looper import Looper 
+    looper = Looper( 'Loop', config, nPrint = 1, nEvents = 1000)
+
+    import time
+    import cProfile
+    p = cProfile.Profile(time.clock)
+    p.runcall(looper.loop)
+    p.print_stats()
+    looper.write()
