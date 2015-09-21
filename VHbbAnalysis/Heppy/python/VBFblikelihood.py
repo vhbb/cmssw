1b4cc6d6c309de1561f17799def1c49673768497
Nadezda <nadezda.chernyavskaya@gmail.com>
2015-09-21 18:04:28 +0200

    variables names are needed to be changed

--- VHbbAnalysis/Heppy/python/VBFblikelihood.py
+++ VHbbAnalysis/Heppy/python/VBFblikelihood.py
@@ -26,20 +26,20 @@ class VBFblikelihood :
 
 
   def evaluateBlikelihood(self, event) :
-    nJet=len(event.jets)#I havent found nJet variable, should I do like this?
-    for index,j in enumerate(event.jets,start=0) :
+    nJet=len(event.jetsForHiggs)#I havent found nJet variable, should I do like this?
+    for index,j in enumerate(event.jetsForHiggs,start=0) :
       j.blike_VBF=-2
-      if j.btagCSV>1 : 
-        j.btagCSV=1
-      if j.btagCSV<0 :
-        j.btagCSV=0
-    if ((event.jets[0].pt()>92.) and (event.jets[1].pt()>76.) and (event.jets[2].pt()>64) and (event.jets[3].pt()>30)):
+      if j.btag >1 : #how is btag called?  
+        j.btag =1
+      if j.btag <0 :
+        j.btag =0
+    if ((event.jetsForHiggs[0].pt()>92.) and (event.jetsForHiggs[1].pt()>76.) and (event.jetsForHiggs[2].pt()>64) and (event.jetsForHiggs[3].pt()>30)):
       if (nJet>=4):
         jetsForHiggs4=[]
-        jetsForHiggs4=[jet for index,jet in enumerate(event.jets) if (jet.jetID()>0) and (index<4) and (jet.pt()>20)]#jetID ,any arguments?
-        btag_max1=max(jetsForHiggs4.btagcsv())        
+        jetsForHiggs4=[jet for index,jet in enumerate(event.jetsForHiggs) if (jet.jetID()>0) and (index<4) and (jet.pt()>20)]#jetID ,any arguments?
+        btag_max1=max(jetsForHiggs4.btag)        
         if btag_max1>0.7:
-          bjet1=jetsForHiggs4[jetsForHiggs4.index.max(jetsForHiggs4.btagcsv())]
+          bjet1=jetsForHiggs4[jetsForHiggs4.index.max(jetsForHiggs4.btag)]
           other3jets=[]
           other3jets=[jet for index,jet in enumerate(jetsForHiggs4) if index!= btag_max1]
           maxEta1, maxEta2 = None, None
@@ -62,20 +62,20 @@ class VBFblikelihood :
             qqDeltaEta=abs(qjets[0].eta()-qjets[1].eta())
 
             if (Mqq>460) and(qqDeltaEta>4.1)and (bbDeltaPhi<1.6) :
-              if (HLT_BIT_HLT_QuadPFJet_SingleBTagCSV_VBF_Mqq460_v==1) : #the name of the trigger variable
+              if (HLT_BIT_HLT_QuadPFJet_SingleBTagCSV_VBF_Mqq460_v==1) : #the name of the trigger variable, how to set it to True, when I run the python vhbb.py it gives a print of triggers and for vbf trigger there are false
 
                 loopMaxJet=7
                 if nJet<7 : loopMaxJet=nJet  
-                jetsForHiggsMax=[jet for index,jet in enumerate(event.jets) if (jet.id>0) and (index<loopMaxJet) and (jet.pt()>20)]
+                jetsForHiggsMax=[jet for index,jet in enumerate(event.jetsForHiggs) if (jet.id>0) and (index<loopMaxJet) and (jet.pt()>20)]
                 jetsEtaIdx=[sorted(range(len(jetsForHiggsMax)),key=lambda x:abs(jetsForHiggsMax[x].eta()))]
-                jetsBtagIdx=[sorted(range(len(jetsForHiggsMax)),key=lambda x:abs(jetsForHiggsMax[x].btagCSV()))]
+                jetsBtagIdx=[sorted(range(len(jetsForHiggsMax)),key=lambda x:abs(jetsForHiggsMax[x].btag))]
 
                 for i  in range(0,loopMaxJet) :
                   j=jetsForHiggsMax[i]
                   if (j.jetID()<=0) or (j.pt()<20) : continue #jetID ,any arguments?
                   self.Jet_pt[i]=j.pt()# [0] or [i]?
                   self.Jet_eta[i]=abs(j.eta())
-                  self.Jet_btagCSV[i]=j.btagcsv()
+                  self.Jet_btagCSV[i]=j.btag
                   self.Jet_pt_idx[i]=i
                   self.Jet_eta_idx[i]=jetsEtaIdx[i]
                   self.Jet_btagCSV_idx[i]=jetsBtagIdx[i]
