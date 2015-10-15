b08ec3b8e8b8da74cb835bb93b60c2d500f23f34
Pierluigi Bortignon <bortigno@gmail.com>
2015-10-15 06:06:30 +0200

    startign development of c++ wrapper

---
+++ VHbbAnalysis/Heppy/interface/ColorFlow.h
@@ -0,0 +1,51 @@
+#ifndef PhysicsTools_Heppy_ColorFlow_h
+#define PhysicsTools_Heppy_ColorFlow_h
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <TVector2.h>
+#include <TLorentzVector.h>
+#include <TMath.h>
+
+#include "DataFormats/PatCandidates/interfce/Jet.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+
+
+namespace heppy{
+class ColorFlow {
+    
+ public:
+ // This class cluster a set of variables that could have sensitivity to color flow connection between two jets
+ // The variable is inspired from this paper: http://arxiv.org/abs/1001.5027
+ //
+ // The class defines a 2D vector (a pull vector) per each jet. The pull vector represents the direction of the radiation in the rapiity-phi plane.
+ // The interface suggests to use the phi of the pull vector (in the rapidity-phi plane) and its magnitude as sensitive variables to color connection.
+
+
+ ColorFlow(const std::vector<pat::Jet> &jets);
+ 
+ float getPullVectorPhi() { return r_mag_; };
+ float getPullVectorMag() { return r_phi_; };
+
+private:
+
+  reco::PFCandidateCollections & pfCands;
+
+  TVector2 calculate_pull_vector_(const std::vector<pat::Jet> &jets) ;
+
+  TVector2 pull_;
+  TVector2 null_;
+  TVector2 ci_;
+  TVector2 r_;
+  TLorentzVector pi_;
+  TLorentzVector J_;
+  float r_mag_;
+  float r_phi_;
+  double patJetpfcPt_;
+  unsigned int nOfConst_;
+
+};
+}
+#endif   
+ 
