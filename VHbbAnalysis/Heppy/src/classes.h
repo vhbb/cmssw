a219002329413d22a262eb06529bf93ec3c3ee6e
Pierluigi Bortignon <ponchwwa@gmail.com>
2015-10-16 19:03:19 -0500

    not working implementation using boost

--- VHbbAnalysis/Heppy/src/classes.h
+++ VHbbAnalysis/Heppy/src/classes.h
@@ -1,9 +1,11 @@
 #include "VHbbAnalysis/Heppy/interface/FastSoftActivity.h"
+#include "VHbbAnalysis/Heppy/interface/ColorFlow.h"
 
 #include <vector>
 namespace {
   struct heppy_dictionaryvhbb {
-    heppy::FastSoftActivity  fs_; 
+    heppy::FastSoftActivity  fs_;
+    heppy::ColorFlow cl_;
 
   };
 }
