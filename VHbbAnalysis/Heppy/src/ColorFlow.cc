40c824393732ab90a20ab54e3be35c5dd0fbaab2
Pierluigi Bortignon <ponchwwa@gmail.com>
2015-10-19 18:47:28 -0500

    First working version of the C++ wrapper for the color flow.

--- VHbbAnalysis/Heppy/src/ColorFlow.cc
+++ VHbbAnalysis/Heppy/src/ColorFlow.cc
@@ -12,59 +12,50 @@ using namespace std;
 
 namespace heppy{
 
-  // ColorFlow::ColorFlow(const std::vector<pat::Jet> &jets):
-  // jets_(jets)
 
-  ColorFlow::ColorFlow(boost::python::object *jets)
+  ColorFlow::ColorFlow(std::vector<math::XYZTLorentzVector> pfCands 
+    : pfCands_ (pfCands)
     {
-      // initialisation - for the moment here, then I'll move it out.
-      max_pfCand_pt = 1;
+      // initalisation of variables
+      init();
 
-      // loop over the jets
-      for( unsigned int i = 0; i<jets->attr("size"); i++ ){
-        // get the number of pfCandidates of a jet
-        unsigned int nOfCands = boost::python::extract<unsigned int>(jets->attr("at")(i).attr("numberOfSourceCandidatePtrs"));
-        // loop over the PF candidate of a jet
-        for ( unsigned int cand_j=0; cand_j < nOfCands; cand_j++){
-          // get a pf candidate
-          reco::CandidatePtr pfCand = boost::python::extract<reco::CandidatePtr> (jets->attr("at")(i).attr("sourceCandidatePtr")(cand_j));
-          if (!pfCand) continue; // check if it is available (?)
-          if (pfCand->charge()==0 or pfCand->pt() < max_pfCand_pt) continue; // calculating a tvector using only charged tracks. This is how it was in the Run1 analysis.
+      // if there are no more than 2 consituents there is no way to calculate the pull Angle - maybe in the future this can be revised.
+      n_of_pf_cands_= pfCands_.size();
+      if (n_of_pf_cands_ < 2) {
+        return;
+      }
+      else { // calculate the pull_vector
 
-          pi_.SetPtEtaPhiE( pfCand->pt(), pfCand->eta(), pfCand->phi(), pfCand->energy() );
-          J_+=pi_; // building the jet using only yhe chared pf candidates four momentum
-          nOfConst_+=1; // count the number of charge constituents
-
-        } // candidate loop
+        // recalculate jet direction using only pfCands_ 
+        J_ = CalculateJetDirection(pfCands_);
         
-        //if there are no more than 2 consituents there is no way to calculate the pull Angle - maybe in the future this can be revised.
-        if (nOfConst_ < 2) t_vector_.Set(0.,0.);
-        else{ // else calculate the t_vector
-
-          for ( unsigned int cand_j=0; cand_j < nOfCands; cand_j++){
-          // get a pf candidate
-          reco::CandidatePtr pfCand = boost::python::extract<reco::CandidatePtr> (jets->attr("at")(i).attr("sourceCandidatePtr")(cand_j));
-          //reco::CandidatePtr pfCand = jets->at(i).sourceCandidatePtr(cand_j);
-          if (!pfCand) continue; // check if it is available (?)
-          if (pfCand->charge()==0 or pfCand->pt() < max_pfCand_pt) continue; // calculating a tvector using only charged tracks. This is how it was in the Run1 analysis.
-
-          pfCand_pt_ = pfCand->pt();
-          pi_.SetPtEtaPhiE(pfCand->pt(),pfCand->eta(),pfCand->phi(),pfCand->energy());
-          r_.Set( pi_.Rapidity() - J_.Rapidity(), deltaPhi( pfCand->phi(), J_.Phi() ) );
+        // calculate the pull vector. detailed explaination of the definition can be found in the paper reference in the header file.
+        for ( unsigned int i=0; i < n_of_pf_cands_; i++){
+          math::XYZTLorentzVector &pfCand = pfCands_.at(i);
+          pf_cand_pt_ = pfCand.Pt();
+          pi_.SetPtEtaPhiE(pfCand.Pt(),pfCand.Eta(),pfCand.Phi(),pfCand.E());
+          r_.Set( pi_.Rapidity() - J_.Rapidity(), deltaPhi( pfCand.Phi(), J_.Phi() ) );
           r_mag_ = r_.Mod();
-          t_vector_ += ( pfCand_pt_ / J_.Pt() ) * r_mag_ * r_;
-
-          } // candidate loop
-
-        } // else
-
-        // now fill the jet information
-        // here I have to ind a way to add the property to the jets from the c++. which kind of objects are these?
-
-      } // jet loop
-
+          pull_vector_ += ( pf_cand_pt_ / J_.Pt() ) * r_mag_ * r_;
+        } // candidate loop
+        return;
+      } // else
     } // constructor
 
+  void ColorFlow::init(){
+      // initialisation of variables      
+      pull_vector_.Set(0.,0.);
+      J_.SetPtEtaPhiE(0.,0.,0.,0.);
+  }
+
+  TLorentzVector ColorFlow::CalculateJetDirection( std::vector<math::XYZTLorentzVector> pfCands_ ){
+    for( unsigned int i = 0; i < n_of_pf_cands_ ; i++ ){
+      math::XYZTLorentzVector &pfCand = pfCands_.at(i);
+      pi_.SetPtEtaPhiE( pfCand.Pt(), pfCand.Eta(), pfCand.Phi(), pfCand.E() );
+      J_+=pi_;
+    }
+    return J_;
+  }
 
 } // namespace heppy
 
