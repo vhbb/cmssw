0423a44e08fb02e2f94935d1742368e51bca4288
Lorenzo <lore.bianchini@gmail.com>
2016-02-02 19:47:51 +0100

    adding two PDF variations for test

--- VHbbAnalysis/Heppy/python/VHbbAnalyzer.py
+++ VHbbAnalysis/Heppy/python/VHbbAnalyzer.py
@@ -56,7 +56,10 @@ class VHbbAnalyzer( Analyzer ):
             self.inputCounterPosWeight = ROOT.TH1F("CountPosWeight","Count genWeight>0",1,0,2)
             self.inputCounterNegWeight = ROOT.TH1F("CountNegWeight","Count genWeight<0",1,0,2)
             for LHE_scale in range(6):
-               setattr(self, "inputCounterWeightedLHEWeight_"+str(LHE_scale), ROOT.TH1F("CountWeightedLHEWeight_"+str(LHE_scale),"Count with gen weight x LHE_weight_scale["+str(LHE_scale)+"] and pu weight",1,0,2))
+               setattr(self, "inputCounterWeightedLHEWeightScale_"+str(LHE_scale), ROOT.TH1F("CountWeightedLHEWeightScale_"+str(LHE_scale),"Count with gen weight x LHE_weights_scale["+str(LHE_scale)+"] and pu weight",1,0,2))
+            for LHE_pdf in range(2):
+               setattr(self, "inputCounterWeightedLHEWeightPdf_"+str(LHE_pdf), ROOT.TH1F("CountWeightedLHEWeightPdf_"+str(LHE_pdf),"Count with gen weight x LHE_weights_pdf["+str(LHE_pdf)+"] and pu weight",1,0,2))
+
         self.regressions={}
 	self.regressionVBF={}
 	for re in self.cfg_ana.regressionVBF :
@@ -437,7 +440,9 @@ class VHbbAnalyzer( Analyzer ):
             genWeight = self.handles['GenInfo'].product().weight()
             self.inputCounterWeighted.Fill(1,copysign(1.0,genWeight)*event.puWeight)
             for LHE_scale in range(6): 
-               getattr(self, "inputCounterWeightedLHEWeight_"+str(LHE_scale)).Fill(1,copysign(1.0, genWeight)*event.puWeight*(event.LHE_weights_scale[LHE_scale]).wgt) 
+               getattr(self, "inputCounterWeightedLHEWeightScale_"+str(LHE_scale)).Fill(1,copysign(1.0, genWeight)*event.puWeight*(event.LHE_weights_scale[LHE_scale]).wgt) 
+            for LHE_pdf in range(2): 
+               getattr(self, "inputCounterWeightedLHEWeightPdf_"+str(LHE_pdf)).Fill(1,copysign(1.0, genWeight)*event.puWeight*(event.LHE_weights_pdf[LHE_pdf]).wgt) 
             if genWeight > 0:
                 self.inputCounterPosWeight.Fill(1)
             elif genWeight < 0:
