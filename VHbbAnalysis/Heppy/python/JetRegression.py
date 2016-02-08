71198130eb8cca960a199aa56e40a45275204844
Lorenzo <lore.bianchini@gmail.com>
2016-02-08 10:54:08 +0100

    fixed divisoon by zero

--- VHbbAnalysis/Heppy/python/JetRegression.py
+++ VHbbAnalysis/Heppy/python/JetRegression.py
@@ -57,13 +57,18 @@ class JetRegression :
 	self.rho[0] = event.rho
 	for j in event.jetsForHiggs :
             pt_corr = 1.0
-            if ("JEC" in analysis and hasattr(j, analysis)):
-                pt_corr = getattr(j, analysis)/getattr(j, "corr") if hasattr(j, "corr") else 1.0
-                #print "JEC"+analysis, pt_corr
-            elif ("JER" in analysis and hasattr(j, analysis)):
-                pt_corr = getattr(j, analysis)/getattr(j, "corrJER") if hasattr(j, "corrJER") else 1.0
-                #print "JER"+analysis, pt_corr
-            if pt_corr<0.:
+            #shifts are implemented by taking the 'nominal' correction and dividing its up/down
+            pt_shift = getattr(j, analysis, -99.)
+            pt_shift_nominal = -99.
+            if pt_shift>0. and "JEC" in analysis:
+                pt_shift_nominal = getattr(j, "corr", -99.)
+                #print "JEC"+analysis, pt_shift_nominal
+            elif pt_shift>0. and "JER" in analysis:
+                pt_shift_nominal = getattr(j, "corrJER", -99.)
+                #print "JEC"+analysis, pt_shift_nominal
+            pt_corr = pt_shift/pt_shift_nominal if pt_shift_nominal>0. else 1.0
+            #print "\t", pt_corr
+            if pt_corr<=0.:
                 pt_corr = 1.0
             self.Jet_pt[0] = j.pt()*pt_corr
             self.Jet_corr[0] = j.rawFactor()/(pt_corr if "JER" not in analysis else 1.0)
