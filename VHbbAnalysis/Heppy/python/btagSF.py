3e90c443688fb6a95889949d8b7a7501045002be
Lorenzo <lore.bianchini@gmail.com>
2016-02-09 11:18:36 +0100

    added latest POG SF and mplemented iterative method in btagSF.py

--- VHbbAnalysis/Heppy/python/btagSF.py
+++ VHbbAnalysis/Heppy/python/btagSF.py
@@ -3,19 +3,39 @@ import os
 
 csvpath = os.environ['CMSSW_BASE']+"/src/VHbbAnalysis/Heppy/data/csv"
 ROOT.gSystem.Load(csvpath+'/BTagCalibrationStandalone.so')
-calib = ROOT.BTagCalibration("csvv2", csvpath+"/CSVv2.csv")
+calib = ROOT.BTagCalibration("csvv2", csvpath+"/CSVv2_prelim.csv")
 
 for wp in [ [0, "L"],[1, "M"], [2,"T"] ]:
     print "CSV"+wp[1]+":"
     for syst in ["central", "up", "down"]:
         print "\t"+syst+":"
         csv_calib_bc = ROOT.BTagCalibrationReader(calib, wp[0], "mujets", syst)
-        csv_calib_l = ROOT.BTagCalibrationReader(calib, wp[0], "comb", syst)
+        csv_calib_l = ROOT.BTagCalibrationReader(calib, wp[0], "incl", syst)
         weight = lambda pt, eta, fl, csv_calib_bc=csv_calib_bc, csv_calib_l=csv_calib_l : (csv_calib_bc.eval( -fl+5 ,eta, pt) if (abs(eta)<2.4 and pt<670. and pt>30.) else 1.0) if fl>=4 else (csv_calib_l.eval(2,eta, pt) if (abs(eta)<2.4 and pt<1000. and pt>20.) else 1.0)
+        # usage: weight( pt, eta, flavour )
         print "\t\tB:", weight(50., 0.5, 5)
         print "\t\tC:", weight(50., 0.5, 4)
         print "\t\tL:", weight(50., 0.5, 0)
 
+def applies( flavour, syst ):
+    if flavour==5 and syst not in ["central", "up_jes", "down_jes",  "up_lf", "down_lf",  "up_hfstats1", "down_hfstats1", "up_hfstats2", "down_hfstats2"]:
+        return False
+    elif flavour==4 and syst not in ["central", "up_cferr1", "down_cferr1", "up_cferr2", "down_cferr2" ]:
+        return False
+    elif flavour==0 and syst not in ["central", "up_jes", "down_jes", "up_hf", "down_hf",  "up_lfstats1", "down_lfstats1", "up_lfstats2", "down_lfstats2" ]:
+        return False
+    return True
+
+print "Iterative:"
+for syst in ["central", "up_jes", "down_jes", "up_lf", "down_lf", "up_hf", "down_hf", "up_hfstats1", "down_hfstats1", "up_hfstats2", "down_hfstats2", "up_lfstats1", "down_lfstats1", "up_lfstats2", "down_lfstats2", "up_cferr1", "down_cferr1", "up_cferr2", "down_cferr2"]:
+    print "\t"+syst
+    csv_calib_iterative = ROOT.BTagCalibrationReader(calib, 3 , "iterativefit", syst)
+    weight = lambda pt, eta, fl, csv, csv_calib=csv_calib_iterative, check=applies, syst=syst : csv_calib.eval( -fl+5, eta, pt, csv ) if abs(eta)<2.4 and pt>20. and pt<10000. and check(fl,syst) else 1.0
+    # usage: weight( pt, eta, flavour, csv )
+    print "\t\tB:", weight(50., 0.5, 5, 0.89)
+    print "\t\tC:", weight(50., 0.5, 4, 0.89)
+    print "\t\tL:", weight(50., 0.5, 3, 0.89)
+    
 
 
 '''
