9e217f9db3eb8c5f7aa00f115174764c15dd9d06
Lorenzo <lore.bianchini@gmail.com>
2016-02-16 12:24:33 +0100

    added latest CSV SF; added cMVSv2 SF

--- VHbbAnalysis/Heppy/python/vhbbobj.py
+++ VHbbAnalysis/Heppy/python/vhbbobj.py
@@ -175,19 +175,28 @@ jetTypeVHbb.variables += [NTupleVariable("bTagWeight",
 )]
 
 # add the POG SF
-ROOT.gSystem.Load(csvpath+'/BTagCalibrationStandalone.so')
-calib = ROOT.BTagCalibration("csvv2", csvpath+"/CSVv2_prelim.csv")
+from btagSF import *
 
-for wp in [ [0, "L"],[1, "M"], [2,"T"] ]:
+for wp in [ "CSVL", "CSVM", "CSVT", "CMVAV2L", "CMVAV2M", "CMVAV2T" ]:
     for syst in ["central", "up", "down"]:
-        csv_calib_bc = ROOT.BTagCalibrationReader(calib, wp[0], "mujets", syst)
-        csv_calib_l = ROOT.BTagCalibrationReader(calib, wp[0], "incl", syst)
-        syst_name = "" if syst=="central" else ("_"+syst.title())
-        jetTypeVHbb.variables += [ NTupleVariable("btagCSV"+wp[1]+"SF"+syst_name,  lambda jet, csv_calib_bc=csv_calib_bc, csv_calib_l=csv_calib_l : 
-                                                  (csv_calib_bc.eval( -jet.hadronFlavour()+5 ,jet.eta(), jet.pt()) if (abs(jet.eta())<2.4 and jet.pt()<670. and jet.pt()>30.) else 1.0) 
-                                                  if jet.hadronFlavour()>=4 
-                                                  else (csv_calib_l.eval(2,jet.eta(), jet.pt()) if (abs(jet.eta())<2.4 and jet.pt()<1000. and jet.pt()>20.) else 1.0)
-                                                  , float, mcOnly=True, help="b-tag CSV"+wp[1]+" POG scale factor, "+syst  )]
+        syst_name = "" if syst=="central" else ("_"+syst.title()) 
+        jetTypeVHbb.variables += [ NTupleVariable("btag"+wp+"SF"+syst_name,  lambda jet, get_csv_SF=get_csv_SF, syst=syst, wp=wp : 
+                                                  get_csv_SF(jet.pt(), jet.eta(), jet.hadronFlavour(), syst, wp)
+                                                  , float, mcOnly=True, help="b-tag "+wp+" POG scale factor, "+syst  )]
+        
+
+#ROOT.gSystem.Load(csvpath+'/BTagCalibrationStandalone.so')
+#calib = ROOT.BTagCalibration("csvv2", csvpath+"/CSVv2.csv")
+#for wp in [ [0, "L"],[1, "M"], [2,"T"] ]:
+#    for syst in ["central", "up", "down"]:
+#        csv_calib_bc = ROOT.BTagCalibrationReader(calib, wp[0], "mujets", syst)
+#        csv_calib_l = ROOT.BTagCalibrationReader(calib, wp[0], "incl", syst)
+#        syst_name = "" if syst=="central" else ("_"+syst.title())
+#        jetTypeVHbb.variables += [ NTupleVariable("btagCSV"+wp[1]+"SF"+syst_name,  lambda jet, csv_calib_bc=csv_calib_bc, csv_calib_l=csv_calib_l : 
+#                                                  (csv_calib_bc.eval( -jet.hadronFlavour()+5 ,jet.eta(), jet.pt()) if (abs(jet.eta())<2.4 and jet.pt()<670. and jet.pt()>30.) else 1.0) 
+#                                                  if jet.hadronFlavour()>=4 
+#                                                  else (csv_calib_l.eval(2,jet.eta(), jet.pt()) if (abs(jet.eta())<2.4 and jet.pt()<1000. and jet.pt()>20.) else 1.0)
+#                                                  , float, mcOnly=True, help="b-tag CSV"+wp[1]+" POG scale factor, "+syst  )]
             
 
 #add per-lepton SF
