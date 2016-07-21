4e025fa8a615b82d9ea87477f6b6c69ae6ee0234
Karl Ehatäht <karlehataht@gmail.com>
2016-07-21 15:54:51 +0300

    Move return statements that prevent filling the Ntuples in VHbbAnalyzer
    If passall is enabled, the 4 lines sometimes caused too early exit in
    the analysis chain and some of the Ntuple branches wouldn't be filled
    correctly because of it. The solution is to check first whether all events
    are supposed to pass the cuts or not (which is the default behavior).

--- VHbbAnalysis/Heppy/python/VHbbAnalyzer.py
+++ VHbbAnalysis/Heppy/python/VHbbAnalyzer.py
@@ -486,10 +486,12 @@ class VHbbAnalyzer( Analyzer ):
 	ssThreshold = 200.
 	# filter events with less than 2 jets with pt 20
         event.jetsForHiggs = [x for x in event.cleanJets if self.cfg_ana.higgsJetsPreSelection(x) ]
-	if not  ( len(event.jetsForHiggs) >= 2  or (len(event.cleanJets) == 1 and event.cleanJets[0].pt() > ssThreshold ) ) :
-		return self.cfg_ana.passall
-        if event.Vtype < 0 and not ( sum(x.pt() > 30 for x in event.jetsForHiggs) >= 4 or sum(x.pt() for x in event.jetsForHiggs[:4]) > 160 ):
-                return self.cfg_ana.passall
+
+        if not self.cfg_ana.passall:
+            if not  ( len(event.jetsForHiggs) >= 2  or (len(event.cleanJets) == 1 and event.cleanJets[0].pt() > ssThreshold ) ) :
+                return False
+            if event.Vtype < 0 and not ( sum(x.pt() > 30 for x in event.jetsForHiggs) >= 4 or sum(x.pt() for x in event.jetsForHiggs[:4]) > 160 ):
+                return False
 
         map(lambda x :x.qgl(),event.jetsForHiggs[:6])
         map(lambda x :x.qgl(),(x for x in event.jetsForHiggs if x.pt() > 30) )
@@ -548,6 +550,7 @@ class VHbbAnalyzer( Analyzer ):
        # Boost(hhh,-Hbalance2.BoostVector())
        # print "unboosted pt",hhh.Pt()
 #	self.makeJets(event,-Hbalance2.BoostVector())
+
         return True
 
 
