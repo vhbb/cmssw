c32b07bd47c015913f56cec37bb15705f5349cf0
perrozzi <perrozzi@cern.ch>
2016-10-25 09:43:31 +0200

    latest updates from david, include for the merger from andrew

--- GeneratorInterface/RivetInterface/plugins/MergedGenParticleProducer.cc
+++ GeneratorInterface/RivetInterface/plugins/MergedGenParticleProducer.cc
@@ -1,4 +1,4 @@
-#include "../plugins/MergedGenParticleProducer.hh"
+#include "GeneratorInterface/RivetInterface/interface/MergedGenParticleProducer.hh"
 #include <vector>
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
