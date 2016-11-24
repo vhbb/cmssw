5d812972c613dc955c8194c80f008be354c5a73f
Joosep Pata <joosep.pata@gmail.com>
2016-11-24 11:56:48 +0100

    rever JEC

--- VHbbAnalysis/Heppy/test/validation/jec_unc.py
+++ VHbbAnalysis/Heppy/test/validation/jec_unc.py
@@ -1,19 +1,22 @@
-import ROOT, sys
+import ROOT, sys, os
 from PhysicsTools.Heppy.physicsutils.JetReCalibrator import JetReCalibrator
 
-files = ["root://storage01.lcg.cscs.ch/pnfs/lcg.cscs.ch/cms/trivcat/store/user/jpata/tth/Nov18_test_v1/ttHTobb_M125_13TeV_powheg_pythia8/Nov18_test_v1/161118_143453/0000/tree_{0}.root".format(ifile) for ifile in range(1, 21)]
+files = os.environ["FILE_NAMES"].split()
+files = ["root://storage01.lcg.cscs.ch//pnfs/lcg.cscs.ch/cms/trivcat/" + fi for fi in files]
 
 of = ROOT.TFile("out.root", "RECREATE")
 tt = ROOT.TChain("vhbb/tree")
 for fi in files:
+    fi = fi.strip()
+    print fi
     tt.AddFile(fi)
 
 of.cd()
-tt.Draw("Jet_pt >>+ nominal(100,0,500)")
-tt.Draw("Jet_pt*Jet_corr >>+ corr(100,0,500)")
+tt.Draw("Jet_pt[0] >>+ nominal(200,20,520)")
+tt.Draw("Jet_pt[0]*Jet_corr[0] >>+ corr(200,20,520)")
 for jet_corr in JetReCalibrator.factorizedJetCorrections + ["JEC"]:
-    s1 = "(Jet_pt * Jet_corr_{0}Up) >>+ h__{0}__Up(100,0,500)".format(jet_corr)
-    s2 = "(Jet_pt * Jet_corr_{0}Down) >>+ h__{0}__Down(100,0,500)".format(jet_corr)
+    s1 = "(Jet_pt[0] * Jet_corr_{0}Up[0]) >>+ h__{0}__Up(200,20,520)".format(jet_corr)
+    s2 = "(Jet_pt[0] * Jet_corr_{0}Down[0]) >>+ h__{0}__Down(200,20,520)".format(jet_corr)
     print s1
     print s2
     tt.Draw(s1)
