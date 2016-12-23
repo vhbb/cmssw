d11b75aa7b5e3c45719c4a5a6647bf9181410744
perrozzi <perrozzi@cern.ch>
2016-12-23 01:19:10 +0100

    detect automatically the pdf to use for the mc2hessian step

--- PhysicsTools/HepMCCandAlgos/plugins/PDFWeightsProducer.cc
+++ PhysicsTools/HepMCCandAlgos/plugins/PDFWeightsProducer.cc
@@ -1,154 +1,231 @@
 /* \class PDFWeightsProducer
- *
- * \author Luca Perrozzi, ETHZ
- *
- * Convert PDF replicas in Hessian representation
- * Originally developed by Josh BEndavid, adapted by Luca Perrozzi
- *
- *
- */
+*
+* \author Luca Perrozzi, ETHZ
+*
+* Convert PDF replicas in Hessian representation
+* Originally developed by Josh BEndavid, adapted by Luca Perrozzi
+*
+*
+*/
 
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/Run.h"
+
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+#include "SimDataFormats/GeneratorProducts/interface/LHERunInfoProduct.h"
 #include "SimDataFormats/GeneratorProducts/interface/LHEEventProduct.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
 #include "PhysicsTools/HepMCCandAlgos/interface/PDFWeightsHelper.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-
-#include "FWCore/Framework/interface/EDProducer.h"
-#include "FWCore/Utilities/interface/InputTag.h"
-#include "DataFormats/Candidate/interface/CandidateFwd.h"
-#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
-#include "SimDataFormats/CrossingFrame/interface/CrossingFrame.h"
-#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
-#include "PhysicsTools/HepMCCandAlgos/interface/MCTruthHelper.h"
-
-#include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
-#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
-
-//#include "SimDataFormats/HiGenData/interface/SubEventMap.h"
-#include "SimDataFormats/CrossingFrame/interface/MixCollection.h"
-
-#include "DataFormats/Common/interface/Handle.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Utilities/interface/EDMException.h"
-#include "FWCore/Utilities/interface/transform.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include <fstream>
-#include <algorithm>
 
 #include <vector>
-#include <string>
-#include <map>
-#include <set>
-
+#include <stdio.h>
+#include <string.h>
+#include <TString.h>
 
 using namespace edm;
-// using namespace reco;
 using namespace std;
-using namespace HepMC;
-
-namespace edm { class ParameterSet; }
-namespace HepMC { class GenParticle; class GenEvent; }
+using namespace lhef;
 
 class PDFWeightsProducer : public edm::EDProducer {
- public:
-  /// constructor
-  PDFWeightsProducer( const edm::ParameterSet & );
-  /// destructor
+public:
+  
+  explicit PDFWeightsProducer( const ParameterSet & cfg ) :
+  _lheRunInfo(consumes<LHERunInfoProduct,edm::InRun>(edm::InputTag("externalLHEProducer"))),
+  srcTokenlheEvent_( consumes<LHEEventProduct>(edm::InputTag("externalLHEProducer"))),
+  srcTokenGen_( consumes<GenEventInfoProduct>(edm::InputTag("generator"))),
+  pdfWeightLHAnumber_(cfg.getParameter<unsigned int>("pdfWeightLHAnumber")),
+  nPdfWeights_(cfg.getParameter<unsigned int>("nPdfWeights")),
+  nPdfEigWeights_(cfg.getParameter<unsigned int>("nPdfEigWeights"))
+  {
+    produces<std::vector<float>>("outputHessianWeights");
+    produces<unsigned int>("inputPDFset");
+    mc2hessianCSV = cfg.getParameter<edm::FileInPath>("mc2hessianCSV");
+  }
+
   ~PDFWeightsProducer();
-
-  /// process one event
-  virtual void produce( edm::Event& e, const edm::EventSetup&) override;
-
- private:
+  
+private:
+  
+  virtual void beginRun(edm::Run const& run, edm::EventSetup const& es);
+  virtual void endRun(edm::Run const& run, edm::EventSetup const& es);
+  virtual void produce( edm::Event&, const edm::EventSetup&);
+  virtual void beginJob();
+  virtual void endJob();
+  
+  edm::FileInPath mc2hessianCSV;
   PDFWeightsHelper pdfweightshelper_;
-  EDGetTokenT<LHEEventProduct> srcToken_;
-  EDGetTokenT<LHEEventProduct> srcTokenAlt_;
+  EDGetTokenT<LHERunInfoProduct> _lheRunInfo;
+  EDGetTokenT<LHEEventProduct> srcTokenlheEvent_;
   EDGetTokenT<GenEventInfoProduct> srcTokenGen_;
-
-  unsigned int pdfWeightOffset_;
+  
+  unsigned int pdfWeightLHAnumber_;
   unsigned int nPdfWeights_;
   unsigned int nPdfEigWeights_;
   
-  std::vector<float> pdfweights_;
-  // std::vector<float> pdfeigweights_;
+  // std::vector<float> pdfweights_;
+  Handle<LHERunInfoProduct> run;
 
   float weight_;
-
+  std::vector<unsigned int> PDFweightsLHEorder_;
+  std::vector<unsigned int> PDFweightsLHEsets_;
+  
 };
 
-
-
-PDFWeightsProducer::PDFWeightsProducer( const ParameterSet & cfg ) :
-  srcToken_( consumes<LHEEventProduct>(edm::InputTag("externalLHEProducer"))),
-  srcTokenAlt_( consumes<LHEEventProduct>(edm::InputTag("source"))),
-  srcTokenGen_( consumes<GenEventInfoProduct>(edm::InputTag("generator"))),
-  pdfWeightOffset_(cfg.getParameter<unsigned int>("pdfWeightOffset")),
-  nPdfWeights_(cfg.getParameter<unsigned int>("nPdfWeights")),
-  nPdfEigWeights_(cfg.getParameter<unsigned int>("nPdfEigWeights")),
-  pdfweights_(nPdfWeights_)
-  // pdfeigweights_(nPdfEigWeights_)
-{
-  
-  produces<std::vector<float>>();
-
-  edm::FileInPath mc2hessianCSV = cfg.getParameter<edm::FileInPath>("mc2hessianCSV");
-  pdfweightshelper_.Init(nPdfWeights_,nPdfEigWeights_,mc2hessianCSV);
+PDFWeightsProducer::~PDFWeightsProducer(){
 }
 
-PDFWeightsProducer::~PDFWeightsProducer() {
+void PDFWeightsProducer::beginJob(){
 }
 
-void PDFWeightsProducer::produce( Event& evt, const EventSetup& es ) {
+void PDFWeightsProducer::beginRun(edm::Run const& iRun, edm::EventSetup const& es) {
 
-    auto_ptr<std::vector<float>> pdfeigweights_ptr( new std::vector<float>() );
-    // pdfeigweights_ = * pdfeigweights_ptr;
-    Handle<LHEEventProduct> lheInfo;
-    evt.getByToken( srcToken_, lheInfo );
-    
-    if (!lheInfo.isValid()) {
-      evt.getByToken( srcTokenAlt_, lheInfo );
+  // Access LHERunInfoProduct (i.e. LHE header) to retrieve the list of weights in the sample
+  iRun.getByLabel( edm::InputTag("externalLHEProducer"), run );
+
+  typedef std::vector<LHERunInfoProduct::Header>::const_iterator headers_const_iterator;
+  LHERunInfoProduct myLHERunInfoProduct = *(run.product());
+  for (headers_const_iterator iter=myLHERunInfoProduct.headers_begin(); iter!=myLHERunInfoProduct.headers_end(); iter++){
+    // cout << "iter->tag()" << iter->tag() << endl;
+    std::vector<std::string> lines = iter->lines();
+    for (unsigned int iLine = 0; iLine<lines.size(); iLine++) {
+      std::string line=lines.at(iLine);
+      TString line_tstr = line.c_str();
+      TString buffer;
+      if (line_tstr.Contains("<weight id=")) {
+        // cout<<iLine<< " "<<line<<endl;
+        line_tstr = line_tstr.ReplaceAll("\n","").ReplaceAll("<weight id=\"","").ReplaceAll("</weight>","");
+        TObjArray *tx = line_tstr.Tokenize("> ");
+        line_tstr = ((TObjString *)(tx->At(tx->GetEntries()-1)))->String();
+        unsigned int pdfnumber = line_tstr.ReplaceAll("PDF set = ","").Atoi();
+        PDFweightsLHEorder_.push_back(pdfnumber);
+        if(pdfnumber%100==1){
+          cout << "PDF SET CANDIDATE FOR REWEIGHTING IN THIS SAMPLE= " << (pdfnumber-1) << endl;
+          PDFweightsLHEsets_.push_back(pdfnumber-1);
+        }
+      }
     }
+  }
 
-    double nomlheweight = lheInfo->weights()[0].wgt;
-
-    Handle<GenEventInfoProduct> genInfo;
-    evt.getByToken( srcTokenGen_, genInfo );
-    
-    weight_ = genInfo->weight();
-    
-    //get the original mc replica weights
-    std::vector<double> inpdfweights(nPdfWeights_);
-    for (unsigned int ipdf=0; ipdf<nPdfWeights_; ++ipdf) {
-      unsigned int iwgt = ipdf + pdfWeightOffset_;
+  // In case this is required, check whether the sample contains the weights 
+  // for the appropriate PDF set to be converted to Hessian.
+  // A check is performed on the NLO NNPDF samples to catch the _pdfas PDF set in case is used
+  if(pdfWeightLHAnumber_<1 || nPdfWeights_<1 || nPdfEigWeights_<1){
+    TString mc2hessianCSV_str = mc2hessianCSV.fullPath();
+    cout << "Either pdfWeightLHAnumber_<1 or nPdfWeights_<1 or nPdfEigWeights_<1: using predefined settings for" << endl;
+    cout << "mc2hessianCSV " << mc2hessianCSV_str << endl;
+    if(mc2hessianCSV_str.Contains("NNPDF30_nlo_as_0118_hessian_60.csv")){
+      if(std::find(PDFweightsLHEsets_.begin(), PDFweightsLHEsets_.end(), 260000) != PDFweightsLHEsets_.end()){
+        // 260000	NNPDF30_nlo_as_0118	      101
+        pdfWeightLHAnumber_ = 260000;
+      }else{
+        // 292200	NNPDF30_nlo_nf_5_pdfas	103	
+        pdfWeightLHAnumber_ = 292200;
+      }
+      nPdfWeights_        = 100;
+      nPdfEigWeights_     = 60;
       
+    }else if(mc2hessianCSV_str.Contains("NNPDF30_nlo_as_0118_nf_4_hessian_60.csv")){
+      if(std::find(PDFweightsLHEsets_.begin(), PDFweightsLHEsets_.end(), 260400) != PDFweightsLHEsets_.end()){
+        // 260400	NNPDF30_nlo_as_0118_nf_4	101	
+        pdfWeightLHAnumber_ = 260400;
+      }else{
+        // 292000	NNPDF30_nlo_nf_4_pdfas	103	
+        pdfWeightLHAnumber_ = 292000;
+      }
+      nPdfWeights_        = 100;
+      nPdfEigWeights_     = 60;
+      
+    }else if(mc2hessianCSV_str.Contains("NNPDF30_lo_as_0130_nf_4_hessian_60.csv")){
+      // 263400	NNPDF30_lo_as_0130_nf_4	  101	
+      pdfWeightLHAnumber_ = 263400;
+      nPdfWeights_        = 100;
+      nPdfEigWeights_     = 60;
+      
+    }else if(mc2hessianCSV_str.Contains("NNPDF30_lo_as_0130_hessian_60.csv")){
+      // 263000	NNPDF30_lo_as_0130	      101	
+      pdfWeightLHAnumber_ = 263000;
+      nPdfWeights_        = 100;
+      nPdfEigWeights_     = 60;
+    }
+    cout 
+    << "pdfWeightLHAnumber= " << pdfWeightLHAnumber_
+    << " nPdfWeights= " << nPdfWeights_
+    << " nPdfEigWeights= " << nPdfEigWeights_
+    << endl;
+  }
+  
+  pdfweightshelper_.Init(nPdfWeights_,nPdfEigWeights_,mc2hessianCSV);
+  
+}
+
+void PDFWeightsProducer::produce( edm::Event & evt, const edm::EventSetup & ){
+
+  auto_ptr<std::vector<float>> pdfeigweights_ptr( new std::vector<float>() );
+  auto_ptr<unsigned int > pdfWeightLHAnumber_ptr( new unsigned int() );
+  Handle<LHEEventProduct> lheInfo;
+  evt.getByLabel( edm::InputTag("externalLHEProducer"), lheInfo );
+  
+  cout << "lheInfo->weights().size()= " << lheInfo->weights().size() << endl;
+  cout << "PDFweightsLHEorder_.size()= " << PDFweightsLHEorder_.size() << endl;
+
+  double nomlheweight = lheInfo->weights()[0].wgt;
+  Handle<GenEventInfoProduct> genInfo;
+  evt.getByToken( srcTokenGen_, genInfo );
+  
+  weight_ = genInfo->weight();
+  
+  //get the original mc replica weights
+  std::vector<double> inpdfweights;
+  for (unsigned int ipdf=0; ipdf<lheInfo->weights().size();ipdf++){
+    
+    if( PDFweightsLHEorder_[ipdf] > pdfWeightLHAnumber_
+        && PDFweightsLHEorder_[ipdf] <= pdfWeightLHAnumber_+nPdfWeights_ ){
+
       //this is the weight to be used for evaluating uncertainties with mc replica weights
-      pdfweights_[ipdf] = lheInfo->weights()[iwgt].wgt*weight_/nomlheweight;
+      // pdfweights_.push_back(lheInfo->weights()[ipdf].wgt*weight_/nomlheweight);
       
       //this is the raw weight to be fed to the mc2hessian convertor
-      inpdfweights[ipdf] = lheInfo->weights()[iwgt].wgt;
+      inpdfweights.push_back(lheInfo->weights()[ipdf].wgt);
+      // foo = std::vector(myVec.begin () + 100000, myVec.begin () + 150000);
       
     }
     
-    std::vector<double> outpdfweights(nPdfEigWeights_);
-    //do the actual conversion, where the nominal lhe weight is needed as the reference point for the linearization
-    pdfweightshelper_.DoMC2Hessian(nomlheweight,inpdfweights.data(),outpdfweights.data());
+    cout << "weight " << inpdfweights.size() 
+    // << " --- PDFmap " << PDFweightsLHEorder_[ipdf] 
+    << " --- PDFmap atoi " << PDFweightsLHEorder_[ipdf]
+    << " --- lheInfo " << lheInfo->weights()[ipdf].wgt 
+    << endl;
     
-    for (unsigned int iwgt=0; iwgt<nPdfEigWeights_; ++iwgt) {
-      double wgtval = outpdfweights[iwgt];
-      
-      //the is the weight to be used for evaluating uncertainties with hessian weights
-      // pdfeigweights_[iwgt] = wgtval*weight_/nomlheweight;
-      pdfeigweights_ptr->push_back(wgtval*weight_/nomlheweight);
-    }    
+    if(inpdfweights.size() == nPdfWeights_) break;
+  }
   
-  evt.put( pdfeigweights_ptr );
+  std::vector<double> outpdfweights(nPdfEigWeights_);
+  //do the actual conversion, where the nominal lhe weight is needed as the reference point for the linearization
+  pdfweightshelper_.DoMC2Hessian(nomlheweight,inpdfweights.data(),outpdfweights.data());
+  
+  for (unsigned int iwgt=0; iwgt<nPdfEigWeights_; ++iwgt){
+    double wgtval = outpdfweights[iwgt];
+    
+    //the is the weight to be used for evaluating uncertainties with hessian weights
+    pdfeigweights_ptr->push_back(wgtval*weight_/nomlheweight);
+    cout << "weight " << pdfeigweights_ptr->size() << " value " << (wgtval*weight_/nomlheweight) << endl;
+  }
+
+  *pdfWeightLHAnumber_ptr = pdfWeightLHAnumber_;
+  
+  evt.put( pdfeigweights_ptr, "outputHessianWeights" );
+  evt.put( pdfWeightLHAnumber_ptr, "inputPDFset" );
 
 }
 
+void PDFWeightsProducer::endRun(edm::Run const& iRun, edm::EventSetup const& es){
+}
+
+void PDFWeightsProducer::endJob(){
+}
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 DEFINE_FWK_MODULE( PDFWeightsProducer );
-
