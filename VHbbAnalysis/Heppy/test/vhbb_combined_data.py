87fa45332abc80dfd6c1431e2defbcbf01d69f7c
Andrea <andrea.rizzi@cern.ch>
2017-01-12 08:38:39 +0100

    rereco input file

--- VHbbAnalysis/Heppy/test/vhbb_combined_data.py
+++ VHbbAnalysis/Heppy/test/vhbb_combined_data.py
@@ -13,7 +13,7 @@ from vhbb_combined import *
 sample.json="json.txt"
 sample.files=[
     #"root://xrootd.ba.infn.it//store/data/Run2015D/BTagCSV/MINIAOD/16Dec2015-v1/50000/00AF8EB4-70AB-E511-9271-00266CFAE7AC.root"
-    "root://131.169.191.218:1094//store/data/Run2016B/BTagCSV/MINIAOD/PromptReco-v2/000/273/537/00000/E8E0EF1B-7E1F-E611-8717-02163E012B71.root",
+    "root://cms-xrd-global.cern.ch//store/data/Run2016B/SingleMuon/MINIAOD/23Sep2016-v1/70000/3AB8DCD4-7387-E611-B30C-0025904CF766.root"
     #"root://131.169.191.218:1094//store/data/Run2016B/SingleMuon//MINIAOD/PromptReco-v2/000/273/537/00000/6EDA3436-A91F-E611-83EA-02163E014736.root"
     ]
 
@@ -21,6 +21,10 @@ TriggerObjectsAna.triggerObjectInputTag = ('selectedPatTrigger','','RECO')
 FlagsAna.processName='RECO'
 TrigAna.triggerBits = triggerTableData
 L1TriggerAna.processName = 'RECO'
+
+import code
+code.interact(local=locals())
+
 
 # and the following runs the process directly 
 if __name__ == '__main__':
