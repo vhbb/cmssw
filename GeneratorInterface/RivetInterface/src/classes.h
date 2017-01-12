88a51cbbe45d112bad02c9703b407e1f4024a2b0
perrozzi <perrozzi@cern.ch>
2017-01-12 15:00:48 +0100

    remove spurious file

--- GeneratorInterface/RivetInterface/src/classes.h
+++
@@ -1,7 +0,0 @@
-#include "GeneratorInterface/RivetInterface/src/HiggsTemplateCrossSections.h"
-
-namespace {
-  struct Rivet_dictionary {
-    Rivet::HiggsClassification  HiggsClassification_; 
-  };
-}
