02ecf2da350b13ff5df83cf9fa79034b9a4bbed4
Andrea <andrea.rizzi@cern.ch>
2017-01-16 15:38:51 +0100

    update GT in cmsRun config

--- VHbbAnalysis/Heppy/test/crab/heppy_crab_config.py
+++ VHbbAnalysis/Heppy/test/crab/heppy_crab_config.py
@@ -2,8 +2,8 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'VHBB_D25_004'
-config.General.workArea = 'crab_projects_D25_004'
+config.General.requestName = 'VHBB_D25_005'
+config.General.workArea = 'crab_projects_D25_005'
 config.General.transferLogs=True
 
 config.section_("JobType")
